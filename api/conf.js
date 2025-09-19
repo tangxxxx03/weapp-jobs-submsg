7b16bdd4884b80fbe1cef0e2e41ca8fbc79df824
小程序 <ylululuxxxxx@gmail.com>
2025-09-19 14:40:55 +0800

    Create conf.js

---
+++ api/conf.js
@@ -0,0 +1,7 @@
+module.exports = async (req, res) => {
+  try{
+    return res.status(200).json({ templateId: process.env.TEMPLATE_ID || "" });
+  }catch(e){
+    return res.status(500).json({ templateId: "" });
+  }
+};
