9a184cdb4c4aa81fde556c871fefeee8f2f60dad
小程序 <ylululuxxxxx@gmail.com>
2025-09-26 16:16:04 +0800

    Delete api directory

--- api/conf.js
+++
@@ -1,8 +0,0 @@
-// api/conf.js
-module.exports = async (req, res) => {
-  try {
-    return res.status(200).json({ templateId: process.env.TEMPLATE_ID || "" });
-  } catch (e) {
-    return res.status(500).json({ templateId: "" });
-  }
-};
